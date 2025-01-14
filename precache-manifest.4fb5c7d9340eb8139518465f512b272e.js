99f6c6a714bd4ee39701b21535e29c51fb87cb25
Kumar Ankur <akakankur81@gmail.com>
2025-01-14 14:44:12 +0530

    Updates

---
+++ precache-manifest.4fb5c7d9340eb8139518465f512b272e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89c4cafaa7e756bd1c6ca1be3da7430",
+    "url": "/netflix_clone/index.html"
+  },
+  {
+    "revision": "7a11f8533ba1449a4b32",
+    "url": "/netflix_clone/static/css/main.f34df743.chunk.css"
+  },
+  {
+    "revision": "e906180625acabddcf76",
+    "url": "/netflix_clone/static/js/2.b8b0d573.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix_clone/static/js/2.b8b0d573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a11f8533ba1449a4b32",
+    "url": "/netflix_clone/static/js/main.a2c9423f.chunk.js"
+  },
+  {
+    "revision": "0a712c896519ee943e70",
+    "url": "/netflix_clone/static/js/runtime-main.b84e150f.js"
+  },
+  {
+    "revision": "032443d69139110b2e85d6802bd81303",
+    "url": "/netflix_clone/static/media/homeview.032443d6.jpg"
+  },
+  {
+    "revision": "2ea85d7448475a744c1485c2eac3d3d1",
+    "url": "/netflix_clone/static/media/netflixLogo.2ea85d74.png"
+  },
+  {
+    "revision": "8666caaae2301629692fb70f8c2771e0",
+    "url": "/netflix_clone/static/media/trailer.8666caaa.mp4"
+  }
+]);
